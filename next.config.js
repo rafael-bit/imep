43e2089c89d989e6ec3ff9766e5ce2992d2d2720
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-06-14 14:30:12 -0300

    fix build errors

--- next.config.js
+++ next.config.js
@@ -41,20 +41,6 @@ const nextConfig = {
 
 		return config;
 	},
-
-	async headers() {
-		return [
-			{
-				source: "/api/:path*",
-				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" },
-					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-				]
-			}
-		]
-	},
 }
 
 module.exports = nextConfig
